b75134515b2462ece69c9ca557830a65365aacfd
Tomasz Dysinski <oz@bunnybones.com>
2014-11-16 22:54:27 -0500

    init commit

---
+++ test.js
@@ -0,0 +1,33 @@
+var PlanetSampler = require('./');
+
+var asciiEmpty = "-";
+var ascii = "░▒▓▓";
+var asciiFull = "█";
+
+var planetSampler = new PlanetSampler();
+
+var rows = 10;
+var layers = 10;
+var cols = 20;
+var scale = .05;
+// var high = -Infinity;
+// var low = Infinity;
+for (var iz = -layers; iz < layers; iz++) {
+	console.log("++++++++++++++++++++++++++++++++++++++++");
+	for (var iy = -rows; iy < rows; iy++) {
+		var outputLine = iy % 2 == 0 ? "." : ",";
+		for (var ix = -cols; ix < cols; ix++) {
+			var val = planetSampler.sample(ix*scale, iy*scale*2, iz*scale*2);
+			// low = low < val ? low : val;
+			// high = high > val ? high : val;
+			if(val == 0) {
+				outputLine += asciiEmpty;
+			} else if (val == 1) {
+				outputLine += asciiFull;
+			} else {
+				outputLine += ascii.charAt(~~(val * (ascii.length-1)));
+			}
+		}
+		console.log(outputLine);
+	};
+}
